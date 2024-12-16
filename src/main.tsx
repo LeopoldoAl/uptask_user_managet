f6c3f58d6f0ea69bf561d3a098ab2d66b6ba1279
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-16 12:10:57 -0400

    Frontend 5

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,6 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello World!</h1>
+    <h1 className='text-6xl text-blue-500'>Hello World!</h1>
   </StrictMode>,
 )
