67c98214dd56a9b924415034539f8295597e4305
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-17 13:54:02 -0400

    Frontend 21

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
 
 
 export default function DashboardView() {
   return (
-    <div>DashboardView</div>
+    <>
+        <h1 
+            className="text-5xl font-black"
+        >
+          My Projects
+        </h1>
+        <p
+            className="text-2xl font-light text-gray-500 mt-5"
+        >
+          Manage your projects
+        </p>
+        <nav className="my-5">
+          <Link
+              className="bg-purple-400
+              hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+              to="/projects/create"
+          >
+            New Project
+          </Link>
+        </nav>
+    </>
   )
 }
