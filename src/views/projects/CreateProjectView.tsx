6ff055ead4ab9806a3a90463f9d47b4d97bebe5b
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-17 13:53:50 -0400

    Frontend 20

---
+++ src/views/projects/CreateProjectView.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+export default function CreateProjectView() {
+  return (
+    <>
+            <h1 
+                className="text-5xl font-black"
+            >
+                Create Projects
+            </h1>
+            <p
+                className="text-2xl font-light text-gray-500 mt-5"
+            >
+                Fill out the next form to create a project
+            </p>
+            <nav className="my-5">
+            <Link
+                className="bg-purple-400
+                hover:bg-purple-500 px-10 py-3 
+                text-white text-xl font-bold cursor-pointer transition-colors"
+                to="/"
+            >
+                Back to Project
+            </Link>
+            </nav>
+    </>
+  )
+}
