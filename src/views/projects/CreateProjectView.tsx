1e758a12d413c330a12f62d353404ae38f0a0bdf
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-17 14:28:03 -0400

    Frontend 24

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,8 +1,19 @@
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form"
+import { Link } from "react-router-dom"
 
 export default function CreateProjectView() {
+    const initialValues = {
+        projectName: "",
+        clientName: "",
+        description: ""
+    }
+    const {register, handleSubmit, formState: {errors}} = useForm({defaultValues: initialValues})
+    const handleForm =  (data) => {
+        console.log(data)
+    }
   return (
     <>
+        <div className="max-w-3xl mx-auto">
             <h1 
                 className="text-5xl font-black"
             >
@@ -14,15 +25,27 @@ export default function CreateProjectView() {
                 Fill out the next form to create a project
             </p>
             <nav className="my-5">
-            <Link
-                className="bg-purple-400
-                hover:bg-purple-500 px-10 py-3 
-                text-white text-xl font-bold cursor-pointer transition-colors"
-                to="/"
-            >
-                Back to Project
-            </Link>
+                <Link
+                    className="bg-purple-400
+                    hover:bg-purple-500 px-10 py-3 
+                    text-white text-xl font-bold cursor-pointer transition-colors"
+                    to="/"
+                >
+                    Back to Project
+                </Link>
             </nav>
+            <form
+                className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+                onSubmit={handleSubmit(handleForm)}
+                noValidate
+            >
+                <input 
+                    type="submit" 
+                    value="Create Project"
+                    className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+                />
+            </form>
+        </div>
     </>
   )
 }
