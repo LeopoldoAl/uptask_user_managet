6a8f70145d92bed55707666eebfe3f98e0ab8476
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-20 10:33:28 -0400

    Frontend 39

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -3,6 +3,7 @@ import ProjectForm from "@/components/projects/ProjectForm"
 import { ProjectFormData } from "@/types/index"
 import { useForm } from "react-hook-form"
 import { Link, useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
 
 export default function CreateProjectView() {
     const navigate = useNavigate()
@@ -13,7 +14,8 @@ export default function CreateProjectView() {
     }
     const {register, handleSubmit, formState: {errors}} = useForm({defaultValues: initialValues})
     const handleForm =  async (data: ProjectFormData) => {
-        await createProject(data)
+        const formData = await createProject(data)
+        toast.success(formData)
         navigate('/')
     }
   return (
