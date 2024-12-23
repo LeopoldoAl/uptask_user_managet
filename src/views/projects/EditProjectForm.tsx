a647d6c9b665118cbd58a306b459ba5fcabd5185
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-23 12:07:18 -0400

    Frontend 64

--- src/views/projects/EditProjectForm.tsx
+++ src/views/projects/EditProjectForm.tsx
@@ -1,13 +1,16 @@
+import { updateProject } from "@/api/ProjectApi";
 import ProjectForm from "@/components/projects/ProjectForm";
-import { ProjectFormData } from "@/types/index";
+import { Project, ProjectFormData } from "@/types/index";
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 type EditProjectFormProps = {
-    data: ProjectFormData
+    data: ProjectFormData,
+    projectId: Project['_id']
 }
 
-export default function EditProjectForm({data}: EditProjectFormProps) {
+export default function EditProjectForm({data, projectId}: EditProjectFormProps) {
         
         const {register, handleSubmit, formState: {errors}} = useForm({defaultValues: {
             projectName: data.projectName,
@@ -15,8 +18,22 @@ export default function EditProjectForm({data}: EditProjectFormProps) {
             description: data.description
         }})
 
+        const { mutate } = useMutation({
+            mutationFn: updateProject,
+            onError: () => {
+
+            },
+            onSuccess: () => {
+
+            }
+        })
+
         const handleForm = (formData: ProjectFormData) => {
-            console.log(formData)
+            const data = {
+                formData,
+                projectId
+            }
+            mutate(data)
         }
   return (
     <>
