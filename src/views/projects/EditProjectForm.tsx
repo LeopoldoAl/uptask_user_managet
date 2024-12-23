65a408e26c92b8c6ddec8afd055177b4aed918e2
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-23 11:12:20 -0400

    Frontend 59

---
+++ src/views/projects/EditProjectForm.tsx
@@ -0,0 +1,59 @@
+import ProjectForm from "@/components/projects/ProjectForm";
+import { ProjectFormData } from "@/types/index";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+
+export default function EditProjectForm() {
+        const initialValues: ProjectFormData = {
+            projectName: "",
+            clientName: "",
+            description: ""
+        }
+        const {register, handleSubmit, formState: {errors}} = useForm({defaultValues: initialValues})
+
+        const handleForm = () => {
+
+        }
+  return (
+    <>
+        <div className="max-w-3xl mx-auto">
+            <h1 
+                className="text-5xl font-black"
+            >
+                Edit Projects
+            </h1>
+            <p
+                className="text-2xl font-light text-gray-500 mt-5"
+            >
+                Fill out the next form to edit the project
+            </p>
+            <nav className="my-5">
+                <Link
+                    className="bg-purple-400
+                    hover:bg-purple-500 px-10 py-3 
+                    text-white text-xl font-bold cursor-pointer transition-colors"
+                    to="/"
+                >
+                    Back to Project
+                </Link>
+            </nav>
+            <form
+                className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+                onSubmit={handleSubmit(handleForm)}
+                noValidate
+            >
+                <ProjectForm
+                    register={register}
+                    errors={errors}
+                />
+                <input 
+                    type="submit" 
+                    value="Save Changes"
+                    className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+                />
+            </form>
+        </div>
+    </>
+  )
+}
