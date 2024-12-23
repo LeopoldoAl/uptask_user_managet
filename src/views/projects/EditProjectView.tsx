7c20dce8721f24c4acd8e39155bc9df79fc7724b
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-23 10:46:30 -0400

    Frontend 58

--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -1,10 +1,18 @@
+import { getProjectById } from "@/api/ProjectApi"
+import { useQuery } from "@tanstack/react-query"
 import { useParams } from "react-router-dom"
 
 
 export default function EditProjectView() {
     const params = useParams()
     const projectId = params.projectId!
-    console.log(projectId)
+    const { data, isLoading, error, isError } = useQuery({
+        queryKey: ['editProject', projectId],
+        queryFn: () => getProjectById(projectId),
+        retry: false
+      })
+
+      console.log(data)
     return (
         <div>EditProjectView</div>
     )
