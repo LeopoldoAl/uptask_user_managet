1205f325d7595153817644ec21471db2caf86b15
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-24 14:47:26 -0400

    Frontend 78

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -1,6 +1,36 @@
+import { getProjectById } from "@/api/ProjectApi"
+import AddTaskModal from "@/components/tasks/AddTaskModal"
+import { useQuery } from "@tanstack/react-query"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 
 export default function ProjectDetailsView() {
-  return (
-    <div>ProjectDetailsView</div>
-  )
+    const navigate = useNavigate()
+    const params = useParams()
+    const projectId = params.projectId!
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ['editProject', projectId],
+        queryFn: () => getProjectById(projectId),
+        retry: false
+      })
+
+      if(isLoading) return 'Loading...'
+      if(isError) return <Navigate to={'/404'}/>
+    if(data) return (
+      <>
+          <h1 className="text-5xl font-black">{data.projectName}</h1>
+          <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
+
+          <nav className="my-5 flex gap-3">
+            <button 
+              className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+              type="button"
+              onClick={() => navigate('?newTask=true')}
+            >
+              Add Task
+            </button>
+          </nav>
+
+          <AddTaskModal/>
+      </>
+    )
 }
