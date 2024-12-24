07fe1e1fddd6015380ea18040785ed9a1889faea
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-24 12:15:42 -0400

    Frontend 72

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,9 +1,10 @@
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
-import { getProjects } from "@/api/ProjectApi"
-import { useQuery } from "@tanstack/react-query"
+import { deleteProject, getProjects } from "@/api/ProjectApi"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { Link } from "react-router-dom"
+import { toast } from 'react-toastify'
 
 
 export default function DashboardView() {
@@ -12,6 +13,17 @@ export default function DashboardView() {
     queryFn: getProjects
   })
 
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation({
+    mutationFn: deleteProject,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data)
+      queryClient.invalidateQueries({queryKey: ['projects']})
+    }
+  })
   if (isLoading) return 'Loading...'
   if (data) return (
     <>
@@ -80,7 +92,7 @@ export default function DashboardView() {
                         <button
                           type='button'
                           className='block px-3 py-1 text-sm leading-6 text-red-500'
-                          onClick={() => { }}
+                          onClick={() => mutate(project._id)}
                         >
                           Delete Project
                         </button>
