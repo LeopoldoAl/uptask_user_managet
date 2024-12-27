447b682c4eacf40b8593eaebd6b4bdb0cca57671
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-27 11:05:36 -0400

    Frontend 87

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -15,6 +15,13 @@ const initialStatus: GroupedTasks = {
   underReview: [],
   completed: []
 }
+const statusTranslation : {[key: string] : string} = {
+  pending: 'Pending',
+  onHold: 'On hold',
+  inProgress: 'In progress',
+  underReview: 'Under Review',
+  completed: 'Completed'
+}
 export default function TaskList({ tasks }: TaskListProps) {
   const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : []
@@ -29,6 +36,11 @@ export default function TaskList({ tasks }: TaskListProps) {
       <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+
+            <h3 
+              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}>
+                {statusTranslation[status]}
+            </h3>
             <ul className='mt-5 space-y-5'>
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">There are not tasks</li>
