a26f254152ebef9967e23ae1caf31e5bea60016e
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-27 11:25:30 -0400

    Frontend 88

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -15,11 +15,18 @@ const initialStatus: GroupedTasks = {
   underReview: [],
   completed: []
 }
+const statusStyles : {[key: string] : string} = {
+  pending: 'border-t-slate-500',
+  onHold: 'border-t-red-500',
+  inProgress: 'border-t-blue-500',
+  underReview: 'border-t-amber-500',
+  completed: 'border-t-emerald-500'
+}
 const statusTranslation : {[key: string] : string} = {
   pending: 'Pending',
   onHold: 'On hold',
   inProgress: 'In progress',
-  underReview: 'Under Review',
+  underReview: 'Under Revision',
   completed: 'Completed'
 }
 export default function TaskList({ tasks }: TaskListProps) {
@@ -28,7 +35,6 @@ export default function TaskList({ tasks }: TaskListProps) {
     currentGroup = [...currentGroup, task]
     return { ...acc, [task.status]: currentGroup }
   }, initialStatus)
-  console.log(groupedTasks)
   return (
     <>
       <h2 className="text-5xl font-black my-10">Task</h2>
@@ -38,7 +44,7 @@ export default function TaskList({ tasks }: TaskListProps) {
           <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
 
             <h3 
-              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}>
+              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}>
                 {statusTranslation[status]}
             </h3>
             <ul className='mt-5 space-y-5'>
