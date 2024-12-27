465f7aedd49edb055fef9f3e5628cf6b1292666d
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-27 12:37:50 -0400

    Frontend 90

---
+++ src/components/tasks/EditTaskData.tsx
@@ -0,0 +1,13 @@
+import { useLocation } from "react-router-dom"
+
+
+export default function EditTaskData() {
+    const location = useLocation()
+    const queryParams = new URLSearchParams(location.search)
+    const editTaskId = queryParams.get('editTask')
+    console.log(editTaskId)
+    return (
+        <div>EditTaskData</div>
+    )
+}
+
