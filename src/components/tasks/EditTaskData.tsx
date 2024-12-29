ccb3a2a4c8ae7279f4528b88a4f708f747003c1b
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-29 14:12:27 -0400

    Frontend 94

--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -1,6 +1,7 @@
 import { getTaskById } from "@/api/TaskAPI"
 import { useQuery } from "@tanstack/react-query"
 import { useLocation, useParams } from "react-router-dom"
+import EditTaskModal from "./EditTaskModal"
 
 
 export default function EditTaskData() {
@@ -12,12 +13,10 @@ export default function EditTaskData() {
     
     const {data} = useQuery({
         queryKey: ['task', taskId],
-        queryFn: () => getTaskById({projectId, taskId})
+        queryFn: () => getTaskById({projectId, taskId}),
+        enabled: !!taskId
     })
 
-    console.log(data)
-    return (
-        <div>EditTaskData</div>
-    )
+    if(data) return <EditTaskModal/>
 }
 
