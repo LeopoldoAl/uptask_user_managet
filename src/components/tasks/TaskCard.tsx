f06409ed7a7d80c549d5a429ce40c8ccb5e1ea49
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-30 12:03:48 -0400

    Frontend 97

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,8 +1,11 @@
+import { deleteTask } from "@/api/TaskAPI"
 import { Task } from "@/types/index"
 import { Menu, Transition } from "@headlessui/react"
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid"
+import { useMutation } from "@tanstack/react-query"
 import { Fragment } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 type TaskCardProps = {
     task: Task
@@ -10,6 +13,19 @@ type TaskCardProps = {
 
 export default function TaskCard({ task }: TaskCardProps) {
     const navigate = useNavigate()
+    const params = useParams()
+    const projectId = params.projectId!
+
+    const {mutate} = useMutation({
+        mutationFn: deleteTask,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+        }
+    })
+
     return (
         <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
             <div className="min-w-0 flex flex-col gap-y-4">
@@ -55,7 +71,11 @@ export default function TaskCard({ task }: TaskCardProps) {
                             </Menu.Item>
 
                             <Menu.Item>
-                                <button type='button' className='block px-3 py-1 text-sm leading-6 text-red-500'>
+                                <button 
+                                    type='button' 
+                                    className='block px-3 py-1 text-sm leading-6 text-red-500'
+                                    onClick={() => mutate({projectId, taskId: task._id})}
+                                >
                                     Delete Task
                                 </button>
                             </Menu.Item>
