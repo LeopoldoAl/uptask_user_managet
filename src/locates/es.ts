64540e4869385b4de3e4bf5cad963fa25ca4799a
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-02 12:39:29 -0400

    Frontend 110

---
+++ src/locates/es.ts
@@ -0,0 +1,7 @@
+export const statusTranslation : {[key: string] : string} = {
+    pending: 'Pending',
+    onHold: 'On hold',
+    inProgress: 'In progress',
+    underReview: 'Under Revision',
+    completed: 'Completed'
+  }
