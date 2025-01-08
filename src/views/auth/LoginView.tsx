71dc69d92d4dc6ae3df5131fd2b0e949cb8914bd
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-08 10:35:03 -0400

    Frontend 120

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,7 +1,71 @@
-
+import { useForm } from "react-hook-form";
+import { UserLoginForm } from "@/types/index";
+import ErrorMessage from "@/components/ErrorMessage";
 
 export default function LoginView() {
+
+  const initialValues: UserLoginForm = {
+    email: '',
+    password: '',
+  }
+  const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+
+  const handleLogin = (formData: UserLoginForm) => { }
+
   return (
-    <div>LoginView</div>
+    <>
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        className="space-y-8 p-10 bg-white"
+        noValidate
+      >
+        <div className="flex flex-col gap-5">
+          <label
+            className="font-normal text-2xl"
+          >Email</label>
+
+          <input
+            id="email"
+            type="email"
+            placeholder="Email Registered"
+            className="w-full p-3  border-gray-300 border"
+            {...register("email", {
+              required: "Email is obligatory!",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Email doesn't valid!",
+              },
+            })}
+          />
+          {errors.email && (
+            <ErrorMessage>{errors.email.message}</ErrorMessage>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <label
+            className="font-normal text-2xl"
+          >Password</label>
+
+          <input
+            type="password"
+            placeholder="Password Registered"
+            className="w-full p-3  border-gray-300 border"
+            {...register("password", {
+              required: "The Password is obligatory!",
+            })}
+          />
+          {errors.password && (
+            <ErrorMessage>{errors.password.message}</ErrorMessage>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          value='Log In'
+          className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3  text-white font-black  text-xl cursor-pointer"
+        />
+      </form>
+    </>
   )
 }
