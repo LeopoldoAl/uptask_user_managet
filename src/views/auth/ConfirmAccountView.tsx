8b98c98bd5d4e62b6d251a3476a2cf4b92916bad
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-08 14:21:08 -0400

    Frontend 126

---
+++ src/views/auth/ConfirmAccountView.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+export default function ConfirmAccountView() {
+
+
+  return (
+    <>
+      <h1 className="text-5xl font-black text-white">Confirm your Account</h1>
+      <p className="text-2xl font-light text-white mt-5">
+      Ingress the code that you have gotten {''}
+        <span className=" text-fuchsia-500 font-bold"> through e-mail</span>
+      </p>
+      <form
+        className="space-y-8 p-10 bg-white mt-10"
+      >
+        <label
+          className="font-normal text-2xl text-center block"
+        >6 digits Code</label>
+
+      </form>
+
+      <nav className="mt-10 flex flex-col space-y-4">
+        <Link
+          to='/auth/new-code'
+          className="text-center text-gray-300 font-normal"
+        >
+          Request a New Code
+        </Link>
+      </nav>
+
+    </>
+  )
+}
