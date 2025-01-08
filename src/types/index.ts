6bbfadd0ae2f9684fb155ec73a974e47f144624e
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-08 10:34:52 -0400

    Frontend 119

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,15 @@
 import { z } from "zod"
 
+/** Auth and Users */
+const authSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  password_confirmation: z.string()
+})
+type Auth = z.infer<typeof authSchema>
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+
 /** Tasks */
 export const taskStatusSchema = z.enum(["pending" , "onHold" , "inProgress" , "underReview" , "completed"])
 export type TaskStatus = z.infer<typeof taskStatusSchema>
