ae0b132811c1a601c44df6e1daddc2bb27c6e237
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-10 14:07:42 -0400

    Frontend 148

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -1,7 +1,18 @@
+import NewPasswordToken from "@/components/auth/NewPasswordToken"
+import NewPasswordForm from "@/components/auth/NewPasswordForm"
+import { useState } from "react"
 
 
 export default function NewPasswordView() {
+  const [ isValidToken, setIsValidToken] = useState(false)
   return (
-    <div>NewPasswordView</div>
+    <>
+      <h1 className="text-5xl font-black text-white">Restore Password</h1>
+      <p className="text-2xl font-light text-white mt-5">
+        Ingress the code that you has received {''}
+        <span className=" text-fuchsia-500 font-bold"> by email</span>
+      </p>
+      {!isValidToken ? <NewPasswordToken/>: <NewPasswordForm/>}
+    </>
   )
 }
