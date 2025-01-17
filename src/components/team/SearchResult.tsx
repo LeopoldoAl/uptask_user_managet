99562f23c6487d0416bd3bdf448cd4070281c8cf
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-17 09:43:19 -0400

    Frontend 168

--- src/components/team/SearchResult.tsx
+++ src/components/team/SearchResult.tsx
@@ -1,10 +1,26 @@
+import { addUserToProject } from "@/api/TeamAPI"
 import { TeamMember } from "@/types/index"
+import { useMutation } from "@tanstack/react-query"
+import { useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 type SearchResultProps = {
     user: TeamMember
 }
 export default function SearchResult({ user }: SearchResultProps) {
-    console.log(user)
+    const params = useParams()
+    const projectId = params.projectId!
+    const { mutate } = useMutation({
+        mutationFn: addUserToProject,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+        }
+    })
+
+    const handleUserAddToProject = () => mutate({projectId, id: user._id})
     return (
         <>
             <p className="mt-10 text-center font-bold">Results</p>
@@ -12,6 +28,7 @@ export default function SearchResult({ user }: SearchResultProps) {
                 <p>{user.name}</p>
                 <button
                     className="text-purple-600 hover:bg-purple-300 px-10 py-3 font-bold cursor-pointer"
+                    onClick={handleUserAddToProject}
                 >Add to the Project</button>
             </div>
         </>
