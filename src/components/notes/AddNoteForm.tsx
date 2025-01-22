df71b4da4502f81a4e63a7c70cbd2c807d4831a7
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-22 09:02:16 -0400

    Frontend 184

--- src/components/notes/AddNoteForm.tsx
+++ src/components/notes/AddNoteForm.tsx
@@ -1,15 +1,32 @@
 import { NoteFormData } from "@/types/index"
 import { useForm } from "react-hook-form"
 import ErrorMessage from "../ErrorMessage"
+import { useMutation } from "@tanstack/react-query"
+import { createNote } from "@/api/NoteAPI"
+import { toast } from "react-toastify"
+import { useLocation, useParams } from "react-router-dom"
 
 export default function AddNoteForm() {
+    const params = useParams()
+    const location = useLocation()
+    const queryParams = new URLSearchParams(location.search)
+    const projectId = params.projectId!
+    const taskId = queryParams.get('viewTask')!
     const initialValues: NoteFormData = {
         content: ""
     }
-    const { register, handleSubmit, formState: {errors} } = useForm({defaultValues: initialValues})
-    const handleAddNote = (formData: NoteFormData) => {
-        console.log(formData)
-    }
+    const { register, handleSubmit, reset, formState: {errors} } = useForm({defaultValues: initialValues})
+    const { mutate } = useMutation({
+        mutationFn: createNote,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            reset()
+        }
+    })
+    const handleAddNote = (formData: NoteFormData) => mutate({formData, projectId, taskId})
   return (
     <form
         onSubmit={handleSubmit(handleAddNote)}
@@ -36,7 +53,7 @@ export default function AddNoteForm() {
         <input 
             type="submit" 
             value={"Create Note"}
-            className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full text-white font-black cursor-pointer"
+            className="bg-fuchsia-600 py-3 hover:bg-fuchsia-700 w-full text-white font-black cursor-pointer"
         />
     </form>
   )
