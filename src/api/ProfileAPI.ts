3fa094cb67f45c84810a6fe79ca3cc4f0bd14ea2
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-23 11:54:53 -0400

    Frontend 193

--- src/api/ProfileAPI.ts
+++ src/api/ProfileAPI.ts
@@ -1,10 +1,20 @@
 import { isAxiosError } from "axios"
-import { UserProfieForm } from "../types"
+import { UpdateCurrentUserPassowrdForm, UserProfieForm } from "../types"
 import api from "@/lib/axios"
 
 export async function updateProfile(formData: UserProfieForm) {
     try {
         const { data } = await api.put<string>(`auth/profile`, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+export async function changePassword(formData: UpdateCurrentUserPassowrdForm) {
+    try {
+        const { data } = await api.post<string>('auth/update-password', formData)
         return data
     } catch (error) {
         if (isAxiosError(error) && error.response) {
