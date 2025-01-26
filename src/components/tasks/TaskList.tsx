0176c7353bcc1b7b739549800ee28d97e43130e5
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-26 10:55:31 -0400

    Frontend 200

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,8 +1,12 @@
 import { statusTranslation } from "@/locates/es"
-import { Task } from "@/types/index"
-import { DndContext } from "@dnd-kit/core"
+import { Task, TaskStatus } from "@/types/index"
+import { DndContext, DragEndEvent } from "@dnd-kit/core"
 import DropTask from "../projects/DropTask"
 import TaskCard from "./TaskCard"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { updateStatus } from "@/api/TaskAPI"
+import { toast } from "react-toastify"
+import { useParams } from "react-router-dom"
 type TaskListProps = {
   tasks: Task[],
   canEdit: boolean
@@ -27,17 +31,38 @@ const statusStyles: { [key: string]: string } = {
 }
 
 export default function TaskList({ tasks, canEdit }: TaskListProps) {
+  const params = useParams()
+  const projectId = params.projectId!
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation({
+    mutationFn: updateStatus,
+    onError: (error) => {
+        toast.error(error.message)
+    },
+    onSuccess: (data) => {
+        toast.success(data)
+        queryClient.invalidateQueries({ queryKey: ['editProject', projectId] })
+    }
+})
   const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : []
     currentGroup = [...currentGroup, task]
     return { ...acc, [task.status]: currentGroup }
   }, initialStatus)
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { over, active } = event
+    if (over && over.id) {
+      const taskId = active.id.toString()
+      const status = over.id as TaskStatus
+      mutate({projectId, taskId, status})
+    }
+  }
   return (
     <>
       <h2 className="text-5xl font-black my-10">Task</h2>
 
       <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
-        <DndContext>
+        <DndContext onDragEnd={handleDragEnd}>
           {Object.entries(groupedTasks).map(([status, tasks]) => (
             <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
 
@@ -45,7 +70,7 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
                 className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}>
                 {statusTranslation[status]}
               </h3>
-              <DropTask />
+              <DropTask status={status}/>
               <ul className='mt-5 space-y-5'>
                 {tasks.length === 0 ? (
                   <li className="text-gray-500 text-center pt-3">There are not tasks</li>
