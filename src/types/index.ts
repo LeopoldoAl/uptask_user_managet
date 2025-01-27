bdeab8febff76b80222da663f1fdeff388e3b7c5
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-27 09:36:57 -0400

    Frontend 204

--- src/types/index.ts
+++ src/types/index.ts
@@ -80,6 +80,11 @@ export const dashboardProjectSchema = z.array(
     manager: true
   })
 )
+export const editProjectSchema = projectSchema.pick({
+  projectName: true,
+  clientName: true,
+  description: true
+})
 export type Project = z.infer<typeof projectSchema>
 export type ProjectFormData = Pick<Project, 'clientName' | 'projectName' | 'description'>
 
